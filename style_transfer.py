75011df6683213688dc3ecbc05403232cd41c86f
oliviachoi <areumchoi6012@gmail.com>
2024-05-22 20:43:24 -0700

    streamlit added

--- style_transfer.py
+++ style_transfer.py
@@ -3,6 +3,9 @@ import tensorflow as tf
 from matplotlib import pyplot as plt
 import numpy as np
 import cv2
+import streamlit as st
+import pandas as pd
+from io import StringIO
 
 model = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
 
@@ -21,3 +24,22 @@ content_image.shape
 stylized_image = model(tf.constant(content_image), tf.constant(style_image))[0]
 
 cv2.imwrite('generated.jpg',cv2.cvtColor(np.squeeze(stylized_image)*255, cv2.COLOR_BGR2RGB) )
+
+
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    st.write(bytes_data)
+
+    # To convert to a string based IO:
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    st.write(stringio)
+
+    # To read file as string:
+    string_data = stringio.read()
+    st.write(string_data)
+
+    # Can be used wherever a "file-like" object is accepted:
+    dataframe = pd.read_csv(uploaded_file)
+    #st.write(dataframe)
